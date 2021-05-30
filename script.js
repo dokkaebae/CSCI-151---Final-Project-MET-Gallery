d40124ed3dc3eaf90beb90b0cb7e1fa22391bf8f
Kirby Ezekiel <kirbyezekielsantos@gmail.com>
2021-05-30 14:35:58 +0800

    Added highlight check.

--- script.js
+++ script.js
@@ -1,50 +1,89 @@
-alert("Select a department from the menu on the left to begin viewing a gallery.");
+let card = document.querySelectorAll(".card");
+card.forEach(c => {
+    c.style.display = "none";
+});
 
+alert("Select a department from the menu on the left to begin viewing a gallery.");
+let response_arr = [];
+let dept = 1;
 let side_btn = document.querySelectorAll(".side-btn");
 let modal = document.querySelector("#modal");
 let modal_img = document.querySelector("#img-modal");
 let modal_close = document.querySelector("#close-modal");
 let card_img = document.querySelectorAll(".card-img");
+let check = document.querySelector(".highlight");
+let load = document.querySelector("#loading");
+
 
 // retrieving images
 
 function get_data(x, obj_ids) {
+    console.log("fetching image data...");
     let ret = fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + obj_ids[x]).then(
         response => {
             if(!response.ok){
                 throw Error(response.statusText);
             }
+            //response;
             return response;
         }
     ).then(json => {
         return json.json();
     }).then(j_response => {
-        if(j_response.isPublicDomain!=false){
+        if(j_response.isPublicDomain!=false && j_response.isHighlight==check.checked) {
+            console.log(j_response.objectID);
             return j_response;
+        } else {
+            return get_data(x+1, obj_ids);
         }
+        
     }).catch(error => {
         console.log(error);
     })
+    
     return ret;
 };
 
-function display_images(obj_ids){
-    let card = document.querySelectorAll(".card");
+function get_info(count, obj_ids){
+    console.log("choosing images...");
     let info_arr = [];
-    let response_arr = [];
     //info_arr = get_data(0, obj_ids, info_arr);
     for(let i = 0; i < obj_ids.length; i++) {
-        if(info_arr.length == 10){
+        if(info_arr.length == 30){
             break;
         } else {
-            Promise.resolve(info_arr.push(get_data(i, obj_ids)));
+            var x = Promise.resolve(get_data(count+i, obj_ids));
+            info_arr.push(x);
             // info_arr.push(get_data(i, obj_ids));
         }
     }
-    Promise.all(info_arr).then(responses => {
-        for(let i = 0; i < 10; i++) {
-            response_arr.push(responses[i]);
+    return info_arr;
+};
+
+function array_eq (arr_1, arr_2) {
+    return Array.isArray(arr_1) && Array.isArray(arr_2) &&
+    arr_1.length === arr_2.length &&
+    arr_1.every((val, index) => val === b[index]);
+};
+
+function display_images(count, obj_ids){
+    console.log("generating images...");
+    Promise.all(get_info(count, obj_ids)).then(responses => {
+        for(let i = 0; i < 30; i++) {
+            if (response_arr.length==10) {
+                break;
+            }
+            if (response_arr.length==0){
+                response_arr.push(responses[i]);
+            } else if(response_arr[response_arr.length-1].objectID !== responses[i].objectID) {
+                console.log(responses[i]);
+                response_arr.push(responses[i]);
+            }
         }
+        if(response_arr.length<10){
+            display_images(count+30, obj_ids);
+        }
+        console.log("done!");
         return response_arr;
     }).then(response_arr => {
         for(i = 0; i < 10; i++) {
@@ -55,14 +94,18 @@ function display_images(obj_ids){
             card[i].childNodes[1].childNodes[3].childNodes[5].textContent = response_arr[i].artistDisplayName;
             card[i].childNodes[1].childNodes[3].childNodes[7].textContent = response_arr[i].artistDisplayBio;
         }
+        card.forEach(c => {
+            c.style.display = "";
+        });
+        load.textContent = "";
     }).catch(error => {
-        console.log("error");
+        console.log(error);
     })
-    console.log(card[0]);
 };
 
 function get_images(dept_id){
-    fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId="+dept_id+"&q=art").then(
+    console.log("searching in department...");
+    let ret = fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId="+dept_id+"&q=art").then(
         response => {
             if(!response.ok){
                 throw Error(response.statusText);
@@ -70,16 +113,24 @@ function get_images(dept_id){
             return response.json();
         }
     ).then(j_response => {
-        display_images(j_response.objectIDs);
+        display_images(0, j_response.objectIDs);
         //return j_response.objectIDs;
     }).catch(error => {
         console.log(error);
-    })
-}
+    });
+    return ret;
+};
 
 function get_department(){
-    let dept_id = this.id;
-    get_images(dept_id);
+    card.forEach(c => {
+        c.style.display = "none";
+    })
+    console.log("retrieving data...")
+    response_arr = [];
+    dept = this.id;
+    check.id = dept;
+    load.textContent = "loading...";
+    get_images(dept);
 };
 
 side_btn.forEach(function(btn) {
@@ -100,6 +151,11 @@ modal_close.onclick = function() {
 };
 
 
+//checkbox
+check.addEventListener("click", get_department);
+
+
 // childnodes: https://www.w3schools.com/jsref/prop_node_childnodes.asp
 // hide scrollbars: https://www.w3schools.com/howto/howto_css_hide_scrollbars.asp
 // modal: https://www.w3schools.com/howto/howto_css_modal_images.asp
+// array comparison: https://masteringjs.io/tutorials/fundamentals/compare-arrays
